20ac4bd3cde31d49e1d5bbe64de74d5ec971c62d
Mustafa Khattab <mustafa.khattab@gmail.com>
2014-03-09 01:07:21 -0800

    Add directory and file find functions

--- elfirm-mode.el
+++ elfirm-mode.el
@@ -3,7 +3,7 @@
 ;; Copyright (c) 2014 Mustafa Khattab
 ;;
 ;; Author: Mustafa Khattab
-;; Version: 0.01
+;; Version: 0.1.0
 ;; Created: 3 Mar 2014
 
 ;; This file is NOT part of GNU Emacs.
@@ -15,7 +15,7 @@
 
 ;;; Code:
 
-(defconst elfirm-version "0.01"
+(defconst elfirm-version "0.1.0"
   "Elfirm mode version.")
 
 (defgroup elfirm nil
@@ -30,11 +30,18 @@ By default this is the parent directory of the Elfirm package."
   :type 'directory)
 
 (defcustom elfirm-categories
-  '((agenda . ("agenda" "latest" "agenda.org"))
-    (documents . ("documents" nil nil))
-    (invoices . ("invoices" "latest" "invoices.org"))
-    (ledger . ("ledger" "latest" "acct.dat"))
-    (reports . ("reports" "latest" nil)))
+  '((agenda . ((:dirname . "agenda")
+               (:default-directory . "latest")
+               (:default-file . "agenda.org")))
+    (documents . ((:dirname . "documents")))
+    (invoices . ((:dirname . "invoices")
+                 (:default-directory . "latest")
+                 (:default-file . "invoices.org")))
+    (ledger . ((:dirname . "ledger")
+               (:default-directory . "latest")
+               (:default-file . "acct.dat")))
+    (reports . ((:dirname . "reports")
+                (:default-directory . "latest"))))
   "Alist defining a client list spec for each category.
 
 Each symbol maps to a spec according to the following format (in
@@ -62,12 +69,21 @@ be selected.")
 
 ;;; Functions:
 
+(defun elfirm-path-join (&rest paths)
+  "Return a concatenated string of PATHS."
+  (apply 'concat (mapcar 'elfirm-f-as-d paths)))
+
+(defalias 'elfirm-p-j 'elfirm-path-join)
+(defalias 'elfirm-f-as-d 'file-name-as-directory)
+
+(defmacro elfirm-category-property (property category)
+  "Return PROPERTY of CATEGORY."
+  `(cdr (assq ,property (cdr (assq ,category elfirm-categories)))))
+
 (defun elfirm-version ()
   "Reports elfirm version number."
   (interactive)
   (message elfirm-version))
-
-(defalias 'elfirm-f-as-d 'file-name-as-directory)
 
 (defun elfirm-str-starts-with-p (str begins)
   "Return non-nil if string STR start with BEGINS."
@@ -79,27 +95,53 @@ be selected.")
 
 (defun elfirm-category-dirname (category)
   "Return CATEGORY directory name."
-  (cadr (assq category elfirm-categories)))
+  (elfirm-category-property :dirname category))
 
 (defun elfirm-locate-category-path (category)
   "Return the directory path for symbol CATEGORY, if exists, otherwise nil."
   (let ((category-path
-         (concat (elfirm-f-as-d elfirm-root-directory)
-                 (elfirm-category-dirname category))))
+         (elfirm-p-j elfirm-root-directory
+                     (elfirm-category-dirname category))))
     (if (file-exists-p category-path)
         (elfirm-f-as-d category-path))))
 
-(defun elfirm-locate-client-path (client category)
+(defun elfirm-locate-client-path (client category &optional use-default directory)
   "Return the client path for string CLIENT within symbol CATEGORY.
 
-If CLIENT does not exist, return nil."
+If CLIENT does not exist, return nil. If USE-DEFAULT is t, then
+append default directory to CATEGORY path. If DIRECTORY is
+non-nil, then find that sub-directory."
   (let* ((category-path
           (elfirm-locate-category-path category))
          (client-path
-          (and category-path (concat category-path client))))
-    (if (and client-path
-             (file-exists-p client-path))
-        (elfirm-f-as-d client-path))))
+          (and category-path (elfirm-p-j category-path client)))
+         (client-default-path           ;append default path if
+                                        ;use-default is non-nil
+          (if use-default
+              (elfirm-p-j client-path (or directory
+                                          (elfirm-category-property
+                                           :default-directory category))))))
+    (if (and client-path (file-exists-p client-path))
+        (cond ((and use-default (file-exists-p client-default-path))
+               client-default-path)
+              (t
+               client-path))
+      nil)))
+
+(defun elfirm-locate-client-file (client category &optional file directory)
+  "Return default file path in CLIENT within CATEGORY default directory.
+
+If FILE is non-nil, then find that file. And if DIRECTORY is
+non-nil, then find FILE within that DIRECTORY. Otherwise, use the
+default directory specified in `elfirm-categories'."
+  (let* ((filename
+         (or file (elfirm-category-property :default-file category)))
+         (client-path
+          (elfirm-locate-client-path client category t directory))
+         (file-path
+          (concat client-path filename)))
+    (if (file-exists-p file-path)
+        file-path)))
 
 (defun elfirm-client-p (client)
   "Return non-nil if CLIENT string is not in ignored list."
@@ -111,12 +153,101 @@ If CLIENT does not exist, return nil."
          (elfirm-locate-category-path category))
         (clients
          (mapcar (lambda (c)
-                   (and
-                    (not (or (string= c ".") (string= c "..")))
-                    (elfirm-client-p c)
+                   (and (elfirm-client-p c)
                     c))
-                 (directory-files category-path))))
+                 (directory-files category-path nil "[^\.\.\.]"))))
     (delq nil clients)))
+
+(defun elfirm-list-categories ()
+  (mapcar (lambda (cat)
+             (symbol-name (car cat)))
+          elfirm-categories))
+
+(defun elfirm-list-files (category client)
+  (let* ((category-path
+          (elfirm-locate-client-path client category t))
+         (files
+          (directory-files category-path nil "[^\.\.\.]")))
+    files))
+
+(defun elfirm-select-category ()
+  (intern-soft (completing-read "category: " (elfirm-list-categories))))
+
+(defun elfirm-select-client (prefix category)
+  (let ((cr-client
+         (lambda ()
+           (completing-read "client: " (elfirm-list-clients category)))))
+    (cond ((equal prefix '(16)) (funcall cr-client))
+          (elfirm-current-client)
+          (t (funcall cr-client)))))
+
+(defun elfirm-select-file (prefix category client)
+  (cond ((equal prefix '(4))
+         (completing-read "file: " (elfirm-list-files category client)))
+        (t
+         (elfirm-category-property :default-file category))))
+
+(defun elfirm-find-category-directory (category client &optional other)
+  "Find CATEGORY directory for CLIENT.
+
+If a command prefix is passed then prompt for other directory.
+OTHER is non-nil, prompt for sub directory."
+  (interactive
+   (let ((category
+          (elfirm-select-category)))
+     (list (elfirm-select-client current-prefix-arg category)
+           category
+           (and (equal current-prefix-arg '(4)) t))))
+  (dired (elfirm-locate-client-path client 'agenda (null other))))
+
+(defun elfirm-find-category-file (category client &optional file)
+  "Find default file in CATEGORY.
+
+If a command prefix is passed, then prompt for other FILE."
+  (interactive
+   (let* ((category
+           (elfirm-select-category))
+          (client
+           (elfirm-select-client current-prefix-arg category))
+          (file
+           (elfirm-select-file current-prefix-arg category client)))
+     (list category client file)))
+  (find-file (elfirm-locate-client-file client category file)))
+
+(defmacro elfirm-deffind-category (name category)
+  "Define an interactive command NAME that will find a CATEGORY."
+  `(defun ,(intern (concat "elfirm-find-category-->" name)) (client &optional other)
+     (interactive
+      (list
+       (elfirm-select-client current-prefix-arg ,category)
+       (and (equal current-prefix-arg '(4)) t)))
+     (elfirm-find-category-directory ,category client other)))
+
+(defmacro elfirm-deffind-file (name category)
+  "Define an interactive command NAME that will find file in CATEGORY."
+  `(defun ,(intern (concat "elfirm-find-file-->" name)) (client &optional file)
+     (interactive
+      (let* ((client
+              (elfirm-select-client current-prefix-arg ,category))
+             (file
+              (elfirm-select-file current-prefix-arg ,category client)))
+        (list client file)))
+     (elfirm-find-category-file ,category client file)))
+
+(elfirm-deffind-category "agenda" 'agenda)
+(elfirm-deffind-category "documents" 'documents)
+(elfirm-deffind-category "invoices" 'invoices)
+(elfirm-deffind-category "ledger" 'ledger)
+(elfirm-deffind-category "reports" 'reports)
+
+(elfirm-deffind-file "agenda" 'agenda)
+(elfirm-deffind-file "documents" 'documents)
+(elfirm-deffind-file "invoices" 'invoices)
+(elfirm-deffind-file "ledger" 'ledger)
+(elfirm-deffind-file "reports" 'reports)
+
+;; (defun elfirm-create-client-direcotry (name &optional categories))
+;; (defun elfirm-create-root-directory ())
 
 (define-minor-mode elfirm-mode
   "Toggle Elfirm mode.
--- elfirm-tests.el
+++ elfirm-tests.el
@@ -13,11 +13,23 @@
                 (elfirm-f-as-d (elfirm-category-dirname 'agenda)))
                (default-directory
                  elfirm-root-directory)
-               (agent-dirname (elfirm-f-as-d "acme")))
-          (make-directory (concat category-dirname agent-dirname) t)
+               (agent-dirs (elfirm-p-j "acme" "latest"))
+               (file-path
+                (concat (elfirm-p-j elfirm-root-directory category-dirname agent-dirs) "agenda.org")))
+          (make-directory (elfirm-p-j category-dirname agent-dirs) t)
+          (append-to-file "" "" file-path)
           (funcall func)))
     (delete-directory elfirm-root-directory t)
     (setq elfirm-root-directory elfirm-test-old-root)))
+
+(ert-deftest elfirm-test-path-join ()
+  "Should return a joined path string."
+  (should
+   (string= (elfirm-path-join "one" "two" "three") "one/two/three/"))
+  (should
+   (string= (elfirm-path-join "one") "one/"))
+  (should
+   (string= (elfirm-path-join) "")))
 
 (ert-deftest elfirm-test-string-starts-with ()
     "Tests the string-starts-with predicate."
@@ -33,7 +45,7 @@
 (ert-deftest elfirm-test-category-dirname ()
   "Should return category directory name, if exists."
   (let ((elfirm-categories
-         '((agenda .  ("agenda" nil nil nil)))))
+         '((agenda .  ((:dirname . "agenda"))))))
       (should (string= (elfirm-category-dirname 'agenda) "agenda"))
       (should (string= (elfirm-category-dirname 'monkey) nil))))
 
@@ -51,12 +63,28 @@
   "Should return path of client within category."
   (elfirm-test-root-dir-fixture
    (lambda ()
-     (let ((client-path
-            (concat (elfirm-f-as-d elfirm-root-directory)
-                    "agenda/acme/")))
-       (should (equal (elfirm-locate-client-path "acme" 'agenda) client-path))
-       (should (equal (elfirm-locate-client-path "monkeys" 'agenda) nil))
-       (should (equal (elfirm-locate-client-path "acme" 'ledger) nil))))))
+     (let* ((client-path
+             (elfirm-p-j elfirm-root-directory "agenda" "acme"))
+            (client-with-default
+             (elfirm-p-j client-path "latest")))
+       (should
+        (equal (elfirm-locate-client-path "acme" 'agenda) client-path))
+       (should
+        (equal (elfirm-locate-client-path "monkeys" 'agenda) nil))
+       (should
+        (equal (elfirm-locate-client-path "acme" 'ledger) nil))
+       (should
+        (equal (elfirm-locate-client-path "acme" 'agenda t) client-with-default))))))
+
+(ert-deftest elfirm-test-locate-client-file ()
+  (elfirm-test-root-dir-fixture
+   (lambda ()
+     (should
+      (equal (stringp (elfirm-locate-client-file "acme" 'agenda "agenda.org")) t))
+     (should
+      (equal (stringp (elfirm-locate-client-file "acme" 'agenda)) t))
+     (should
+      (equal (elfirm-locate-client-file "acme" 'agenda "monkey.org") nil)))))
 
 (ert-deftest elfirm-test-list-clients ()
   "Should return a list of clients within a category."
@@ -64,3 +92,18 @@
    (lambda ()
      (let ((client-list '("acme")))
        (should (equal (elfirm-list-clients 'agenda) client-list))))))
+
+(ert-deftest elfirm-test-list-categories ()
+  "Should return a list of categories."
+  (let ((elfirm-categories
+         '((agenda .  ((:dirname . "agenda")))
+           (monkeys . ((:dirname . "monkeys"))))))
+    (should
+     (equal (elfirm-list-categories) '("agenda" "monkeys")))))
+
+(ert-deftest elfirm-test-list-files ()
+  "Should return a list of files within a client category."
+  (elfirm-test-root-dir-fixture
+   (lambda ()
+     (should
+      (equal (elfirm-list-files 'agenda "acme") '("agenda.org"))))))
